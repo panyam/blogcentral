f6e6badd7a5b5f0116c2909424a1dfce5b2f25f8
Sriram Panyam <sri.panyam@gmail.com>
2020-04-13 22:03:35 -0700

    Removed unused files

--- gdocs/project/server/Main.js
+++ gdocs/project/server/Main.js
@@ -17,64 +17,13 @@ function onOpen() {
     Logger.log("Active Sheet: ", activeSheet);
     SpreadsheetApp.getUi()
     .createAddonMenu()
-    .addItem("Start", "showStartDialog")
-    .addItem("Refresh", "refreshTracker")
-    .addItem("Reset", "clearCurrentSheet")
+    .addItem("Start", "startBlogCentral")
     .addToUi();
 }
 
-/**
- * Called when a cell is edited.
- */
-function onEdit(e) {
-    var tracker = getActiveTracker()
-    if (tracker == null) {
-      Logger.log("Active Tracker not found: ", tracker, TRACKERS_BY_SHEET);
-      return ;
-    }
-    var col = e.range.getColumn();
-    var row = e.range.getRow();
-    var numRows = e.range.getNumRows();
-    var numCols = e.range.getNumColumns();
-    
-    Logger.log("Curr Row: " + row + ", Curr Col: " + col);
-    if (tracker.isDaterangeCell(row, col)) {
-        tracker.redrawCalendar();
-    } else {
-        // var formulas = range.getFormulas();
-        var updatedTaskRows = tracker.taskRowsInRange(row, col, numRows, numCols);
-        tracker.highlightTaskRows(updatedTaskRows);
-    }
-}
-
-function refreshTracker() {
-    var tracker = getActiveTracker()
-    if (tracker == null) {
-      SpreadsheetApp.getUi().alert("You have not yet created a tracker for this sheet.  Click on Add-ons > Blog Central > Start");
-    } else {
-      tracker.redrawProject();
-      tracker.redrawCalendar();
-    }
-}
-
-/**
- * Clear the current sheet erasing everythign.  The tracker associated with the current sheet is also removed.
- */
-function clearCurrentSheet() {
-  var ui = SpreadsheetApp.getUi();
-  var result = ui.alert("All data, formulas and formatting will be reset.  Are you sure?", ui.ButtonSet.YES_NO);
-  if (result == ui.Button.NO) {
-    return ;
-  }
-  
-  var sheet = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet();
-  removeTrackerForSheet(sheet);
-  sheet.clear();
-}
-
-function showStartDialog() {
+function startBlogCentral() {
   var html = HtmlService.createTemplateFromFile("client/main")
     .evaluate()
-    .setTitle("Blog Central - Options"); // The title shows in the sidebar
+    .setTitle("Blog Central"); // The title shows in the sidebar
   SpreadsheetApp.getUi().showModalDialog(html, "Start Tracker in this Sheet"); // userInterface, title)showSidebar(html);
 }
