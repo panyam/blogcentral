1c6a5db8a459dbb70ba3808e76ca3ae6ee50cdd0
Sriram Panyam <sri.panyam@gmail.com>
2020-05-12 01:58:41 -0700

    Quill still busted

--- gdocs/client/lib/flask.ts
+++ gdocs/client/lib/flask.ts
@@ -61,11 +61,38 @@ export class LocalExtractor implements ContentExtractor {
   elemid: string;
   quill: any;
 
+  get toolbarOptions() {
+        return [
+    // [{ 'font': fonts }],
+    // [{ 'header': 1 }, { 'header': 2 }],
+    [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+    [{ 'size': ['small', false, 'large', 'huge'] }],
+    ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+    ['blockquote', 'code-block'],
+
+    [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+    // [{ 'script': 'sub'}, { 'script': 'super' }],
+    // [{ 'indent': '-1'}, { 'indent': '+1' }],
+    // [{ 'direction': 'rtl' }],
+
+    
+    [{ 'color': [] }, { 'background': [] }],
+    
+    // [{align: []}],
+    // [{ 'align': [false, 'right', 'center', 'justify'] }],
+    [{align: ''}, {align: 'right'}, {align: 'center'}, {align: 'justify'}],
+    ['image', 'video'],
+    ['clean'],                                         // remove formatting button
+    ['fullscreen']
+  ];
+  }
+
   constructor(elemid: string) {
     this.elemid = elemid;
     this.quill = new Quill("#" + elemid, {
       theme: "snow",
       debug: "info",
+      placeholder: 'Enter Post Content Here...',
       modules: { toolbar: true } // "#quill_editor > #toolbar", },
     });
   }
